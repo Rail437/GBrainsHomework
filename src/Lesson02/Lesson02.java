0f68c1ebfc357d860245500b0185615ee9d23d01
Rail437 <opel808437@gmail.com>
2021-03-23 14:39:12 +0300

    lesson02_v4

--- src/Lesson02/Lesson02.java
+++ src/Lesson02/Lesson02.java
@@ -1,11 +1,14 @@
 package Lesson02;
 
+import java.util.Arrays;
+
 public class Lesson02 {
     public static void main(String[] args) {
         int [] array_one = {0,0,1,1,0,1,0,0,1};
         int [] array_null = new int [8];
         int [] array_x2 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         int [][] arr_square = new int [7][7];
+        int [] arr_box = {1,2,3,4,5};
 
         print(array_one);
         print(array_null);
@@ -15,44 +18,15 @@ public class Lesson02 {
         arr_min_max(array_x2);
         arr_x2(array_x2);
         diagonals(arr_square);
-        print_matrix(arr_square);
+
         print(array_one);
         print(array_null);
         print(array_x2);
+        print_matrix(arr_square);
+        shift(arr_box, 2);
+        print(arr_box);
     }
-    public static void diagonals(int [][] arr) {
-        int count = 0;
-        int count_rev = 0;
-        for (int i = 0; i < arr.length; i ++){
-            for (int j = 0; j < arr[i].length; j++){
-                if(count == j) {
-                    arr[i][j] = 1;
-                }
-                count_rev = j;
-            }
-            arr[i][count_rev - count] = 1;
-            count++;
-        }
-    }
-    public static void print_matrix (int [][] arr) {
-        for (int i = 0; i < arr.length; i ++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j] + " | ");
-            }
-            System.out.println();
-        }
-    }
-    /*int i = 0;
-    int j = 0;
-        while (i < arr.length){
-        arr[0] = 1;
-        j = 0;
-        while (j < arr[i].length){
-            if( )
-                j++;
-        }
-        i++;
-    }*/
+
     public static void swap (int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 0) {
@@ -91,5 +65,40 @@ public class Lesson02 {
             System.out.print(array[j] + " | ");
         }
         System.out.println();
+    }
+    public static void diagonals(int [][] arr) {
+        int count = 0;
+        int count_rev = 0;
+        for (int i = 0; i < arr.length; i ++){
+            for (int j = 0; j < arr[i].length; j++){
+                if(count == j) {
+                    arr[i][j] = 1;
+                }
+                count_rev = j;
+            }
+            arr[i][count_rev - count] = 1;
+            count++;
+        }
+    }
+    public static void print_matrix (int [][] arr) {
+        for (int i = 0; i < arr.length; i ++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " | ");
+            }
+            System.out.println();
+        }
+    }
+    public static void shift(int [] arr, int count){
+        int [] arrbuff = Arrays.copyOf(arr, arr.length);
+        int j = count;
+        if (count < 0){
+            j = arr.length + count;
+        }
+        for (int i = 0; i < arr.length; i++){
+            if (j >= arr.length) {
+                j = 0;
+            }
+            arr[j++] = arrbuff[i];
+        }
     }
 }
