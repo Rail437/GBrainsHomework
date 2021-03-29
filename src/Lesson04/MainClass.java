52f81e18364886894b7cfa4999eba54b00f307b8
Rail437 <opel808437@gmail.com>
2021-03-29 12:52:16 +0300

    Пробный коммит.

--- src/Lesson04/MainClass.java
+++ src/Lesson04/MainClass.java
@@ -1,4 +1,33 @@
 package Lesson04;
 
+
 public class MainClass {
+    public static void main (String [] args){
+        Worker person1 = new Worker();
+        //Worker [] arrayWorkers = new Worker[5];
+        //arrayWorkers[0] = arrayWorkers[0].setName("Bibo");
+        //arrayWorkers[0] = arrayWorkers[0].setPost("Cleaner");
+        //arrayWorkers[0] = arrayWorkers[0].setSalary(10000);
+        //System.out.println("Worker's name " + arrayWorkers[0].getName() + ", post " + arrayWorkers[0].getPost());
+
+    /*for (int i = 0; i < arrayWorkers.length; i++){
+        if (arrayWorkers[i].getAge() > 40){
+            System.out.print("Worker's name " + arrayWorkers[i].getName() + ", post " + arrayWorkers[i].getPost());
+            System.out.print(", pthone namber: " + arrayWorkers[i].getPhoneNumber() + ", salary: " + arrayWorkers[i].getSalary());
+            System.out.print(", age: " + arrayWorkers[i].getAge() + ", ID: " + arrayWorkers[i].getId());
+            System.out.println();
+        }
+    }
+    upSalary(arrayWorkers);*/
+        //System.out.println("Worker's name: " + person1.getName() + ", post: " + person1.getPost());
+
+    }
+
+    private static void upSalary (Worker[] person ){
+        for (int i = 0; i < person.length; i++) {
+            if (person[i].getAge() > 35) {
+                person[i].setSalary(person[i].getSalary() + 10000);
+            }
+        }
+    }
 }
--- src/Lesson04/Worker.java
+++ src/Lesson04/Worker.java
@@ -1,4 +1,40 @@
 package Lesson04;
 
 public class Worker {
+    private String name;
+    private String post;
+    private int phoneNumber;
+    private int salary;
+    private int age;
+    private int id;
+    private static int idBox = 1;
+
+    public  Worker () {
+        this.name = "default name";
+        this.post = "default post";
+        this.phoneNumber = 0 ;
+        this.salary = 0;
+        this.age = 18;
+        this.id = idBox++;
+    }
+    public Worker(String name, String post, int phoneNumber, int salary, int age){
+        this.name = name;
+        this.post = post;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+        this.id = idBox++;
+    }
+    //GEtter
+    public String getName() { return name; }
+    public String getPost() { return post; }
+    public int getPhoneNumber() { return phoneNumber; }
+    public int getSalary() { return salary; }
+    public int getAge () { return age; }
+    public int getId () { return id;}
+
+    //Setter
+    public void setSalary (int salary) {this.salary = salary; }
+    public void setName (String name) { this.name = name;}
+    public void setPost (String post) { this.post = post;}
 }
