baa85fb1e617fdacf09bc5b7691e8980f48a58dc
Rail437 <opel808437@gmail.com>
2021-03-30 23:05:31 +0300

    Lesson04

--- src/Lesson04/Worker.java
+++ src/Lesson04/Worker.java
@@ -3,21 +3,21 @@ package Lesson04;
 public class Worker {
     private String name;
     private String post;
-    private int phoneNumber;
+    private long phoneNumber;
     private int salary;
     private int age;
     private int id;
-    private static int idBox = 1;
+    private static int idBox = 1000000;
 
     public  Worker () {
         this.name = "default name";
         this.post = "default post";
-        this.phoneNumber = 0 ;
+        this.phoneNumber = 88005553535L ;
         this.salary = 0;
         this.age = 18;
         this.id = idBox++;
     }
-    public Worker(String name, String post, int phoneNumber, int salary, int age){
+    public Worker(String name, String post, long phoneNumber, int salary, int age){
         this.name = name;
         this.post = post;
         this.phoneNumber = phoneNumber;
@@ -28,7 +28,7 @@ public class Worker {
     //GEtter
     public String getName() { return name; }
     public String getPost() { return post; }
-    public int getPhoneNumber() { return phoneNumber; }
+    public long getPhoneNumber() { return phoneNumber; }
     public int getSalary() { return salary; }
     public int getAge () { return age; }
     public int getId () { return id;}
