604d80de36b7106199abd901bdb89cafc0921c9a
Rail <opel808437@gmail.com>
2021-04-05 20:25:15 +0300

    Последнее задание еще не готово.

--- src/Lesson06/MainClass.java
+++ src/Lesson06/MainClass.java
@@ -1,6 +1,7 @@
 package Lesson06;
 
 import java.io.*;
+import java.util.Locale;
 import java.util.Random;
 
 public class MainClass {
@@ -8,11 +9,9 @@ public class MainClass {
         String fileName = "src/Lesson06/Home Work1.txt";
         String fileName2 = "src/Lesson06/Home Work2.txt";
         Boolean selector = false;
-        //String text = "";
-        //String text2 = "";
-        //int count = 100;
-        //randomText(text, count);
-        String text = "STAR WARS\n Episode 4\n" + "New Hope\n" +
+        String checkWord = "Hope";
+
+        String text1 = "STAR WARS\nEpisode 4\n" + "New Hope\n" +
         "There is a civil war. Spaceships\n" +
         "insurgents striking from a secret base,\n" +
         "won the first victory in the battle\n" + "with the sinister Galactic Empire.\n";
@@ -22,24 +21,12 @@ public class MainClass {
         "a station capable of destroying an entire planet.\n" +
         "Princess Chased by Imperial Agents\n" +
         "Leia hurries home in her starship.";
-        //text2 = text2 + randomText(text2, count);
-        writeToFile(fileName, text, selector);
+        writeToFile(fileName, text1, selector);
         writeToFile(fileName2, text2,selector);
         mixToFile(fileName, fileName2);
+        System.out.println(checkWord(checkWord, fileName));
+        System.out.println(searchFiles(new File("D:\\"),checkWord));
     }
-    /*private static String randomText(String text1, int count){
-        Random random = new Random();
-
-        for (int i = 0; i < count; i++){
-            int rand = random.nextInt(58) + 65;
-            if (rand > 90 && rand < 97){
-                text1 = text1 + 'X';
-            }else text1 = text1 + (char)rand;
-        }
-        return text1;
-
-
-    }*/
     private static void writeToFile (String nameFile, String text, boolean selector){
         try {
             FileOutputStream fileOutputStream = new FileOutputStream(nameFile, selector);
@@ -61,22 +48,22 @@ public class MainClass {
             String mixedFile = "src/Lesson06/MixFile.txt";
             int tmp;
             int ss = 0;
-            String bob;
+            String buff;
             boolean sel = false;
 
             while ((tmp = file1InputStream.read()) != -1){
                 if(ss > 0){
                     sel = true;
                 }
-                bob = "" + (char)tmp;
-                writeToFile(mixedFile, bob, sel);
+                buff = "" + (char)tmp;
+                writeToFile(mixedFile, buff, sel);
                 ss++;
             }
             file1InputStream.close();
             FileInputStream file2InputStream = new FileInputStream(file2);
             while ((tmp = file2InputStream.read()) != -1){
-                bob = "" + (char)tmp;
-                writeToFile(mixedFile, bob, true);
+                buff = "" + (char)tmp;
+                writeToFile(mixedFile, buff, true);
             }
             file2InputStream.close();
         }catch (FileNotFoundException fileNotFoundException) {
@@ -85,7 +72,52 @@ public class MainClass {
             System.out.println("ERROR: IOExeption.");
         }
     }
-    private static void checkWord (String word, String file){
-
+    private static boolean checkWord (String word, String file){
+    try{
+        int count = word.length();
+    int tmp;
+    int i = 0;
+    char[] buff = word.toCharArray();
+    //buff = word;
+        FileInputStream fileInputStream = new FileInputStream(file);
+        while ((tmp = fileInputStream.read()) != -1){
+            if((char)tmp == buff[i]){
+                //System.out.print(buff[i]);
+                i++;
+            }else
+                i = 0;
+            if(i == count){
+                return true;
+            }
+        }
+        fileInputStream.close();
+    }catch (FileNotFoundException fileNotFoundException) {
+        System.out.println("ERROR: file not found.");
+    }catch (IOException exception) {
+        System.out.println("ERROR: IOExeption.");
+    }catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException) {
+        System.out.println("ERROR: No search word entered.");
+    }
+    return false;
+    }
+    private static boolean searchFiles(File rootFile, String word){
+        if(rootFile.isDirectory()){
+            System.out.println("searching at: " + rootFile.getAbsolutePath());
+            File [] directoryFiles = rootFile.listFiles();
+            if(directoryFiles != null){
+                for (File file: directoryFiles){
+                    if(file.isDirectory()){
+                        searchFiles(file,word);
+                    } else {
+                        if(file.getName().toLowerCase().endsWith(".txt")){
+                        if(checkWord(word,file.getAbsolutePath()) == true){
+                            return true;
+                        }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
