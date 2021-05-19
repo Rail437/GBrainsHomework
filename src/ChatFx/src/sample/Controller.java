71f84000ee764ecd674c5e78f1b8cbbee60f3ae7
RailYunusov <opel808437@gmail.com>
2021-05-20 01:51:32 +0300

    Сделал чатик который мы разбирали на уроке. Но использовал JavaFx. ДОбавил дополнительное поле где можно видеть ники тех людей кто сейчас онлайн.

---
+++ src/ChatFx/src/sample/Controller.java
@@ -0,0 +1,149 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+
+import javax.swing.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.*;
+
+
+public class Controller {
+    private static Socket socket;
+    public static DataInputStream inputStream;
+    public static DataOutputStream outputStream;
+
+    @FXML
+    private TextArea messeges;
+    @FXML
+    private TextArea nickList;
+    @FXML
+    private TextField inputText;
+
+
+    @FXML
+    public void openConnection() throws IOException {
+        socket = new Socket(ChatConstants.HOST, ChatConstants.PORT);
+        inputStream = new DataInputStream(socket.getInputStream());
+        outputStream = new DataOutputStream(socket.getOutputStream());
+
+        new Thread(() -> {
+            try {
+                //auth
+                while (true) {
+                    String strFromServer = inputStream.readUTF();
+                    if (strFromServer.equals(ChatConstants.AUTH_OK)) {
+                        break;
+                    }
+                    if (strFromServer.startsWith(ChatConstants.SEND_REFRESH_LIST)){
+                        this.nickList.clear();
+                        String str = Arrays.stream(strFromServer.split("\\s+")).skip(1).map((s -> s + "\n"))
+                        .collect(Collectors.joining());
+                        this.nickList.appendText(str);
+                        continue;
+                    }
+                    this.messeges.appendText(strFromServer);
+                    this.messeges.appendText("\n");
+                }
+                //read
+                while (true) {
+
+                    String strFromServer = inputStream.readUTF();
+                    if (strFromServer.equals(ChatConstants.STOP_WORD)) {
+                        break;
+                    }
+                    this.messeges.appendText(strFromServer);
+                    this.messeges.appendText("\n");
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }).start();
+    }
+
+    public static void closeConnection() {
+        try {
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void onClickButton(javafx.event.ActionEvent event) {
+        try {
+            if (inputText.getText().equals(ChatConstants.CONNECT)) {
+                openConnection();                      //Тут подключение к серверу.
+                inputText.setText("");
+            }
+            if (inputText.getText().equals(ChatConstants.DISCONNECT)) {
+                ending();                              //Тут отключаемся
+                inputText.setText("");
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+            inputText.setText("");
+        }
+        if (!inputText.getText().trim().isEmpty()) {
+            sendMessage();
+        }
+    }
+
+    private void sendMessage() {
+        if (!inputText.getText().trim().isEmpty()) {
+            try {
+                outputStream.writeUTF(inputText.getText());
+                inputText.setText("");
+                inputText.fontProperty();
+            } catch (IOException e) {
+                e.printStackTrace();
+                inputText.setText("");
+                JOptionPane.showMessageDialog(null, "Send error occured");
+            }
+        }
+    }
+
+    private void sendMessage(String text) {
+        try {
+            outputStream.writeUTF(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Send error occured");
+        }
+    }
+
+    protected static void ending() {
+        try {
+            outputStream.writeUTF(ChatConstants.STOP_WORD);
+            closeConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void refreshList(ActionEvent event) {
+        try {
+            outputStream.writeUTF(ChatConstants.SEND_REFRESH_LIST);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
